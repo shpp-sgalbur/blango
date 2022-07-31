c67801d2ca5c9ee8f1ba64c417cb51dfef35cbe1
Сергей Галбур <galsergey20@gmail.com>
2022-07-31 16:38:46 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -30,6 +30,9 @@ class Dev(Configuration):
     DEBUG = True
 
     AUTH_USER_MODEL = "blango_auth.User"
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
+    
 
     ALLOWED_HOSTS = ['*']
     X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
